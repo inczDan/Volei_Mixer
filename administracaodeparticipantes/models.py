59aa28087b912bf40fab4de17e552f7f925ae4d0
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2022-08-09 15:53:39 -0300

    rendering tables

--- administracaodeparticipantes/models.py
+++ administracaodeparticipantes/models.py
@@ -4,11 +4,13 @@ from django.db import models
 from django.forms import ModelForm
 
 hability_value = (
-    (1, 'Excelente'),
-    (2, 'Bom'),
-    (3, 'Intermediário'),
-    (4, 'Em desenvolvimento'),
+    (1, 'Excelente(1)'),
+    (2, 'Bom(2)'),
+    (3, 'Intermediário(3)'),
+    (4, 'Em desenvolvimento(4)'),
 )
+
+# renomear para Participante
 class CadastrarParticipantes(models.Model):
     nome = models.CharField(max_length=20)
     sobrenome = models.CharField(max_length=20)
@@ -16,8 +18,14 @@ class CadastrarParticipantes(models.Model):
 
     def __str__(self):
         return self.nome
+    
+    @property
+    def habilidade_str(self):
+        h = [text for (idx, text) in hability_value if idx == self.habilidade]
+        return h[0]
 
 
+# isto deveria estar no forms.py TIRAR DAQUI
 class CadastrarPartipantesForms(ModelForm):
     class Meta:
         model = CadastrarParticipantes
--- administracaodeparticipantes/views.py
+++ administracaodeparticipantes/views.py
@@ -1,8 +1,17 @@
+from multiprocessing import context
 from django.shortcuts import render
-from .models import CadastrarParticipantes
-from django.views.generic.list import ListView
+from .models import CadastrarParticipantes, CadastrarPartipantesForms
 
 
 def create(request):
-    playerid = int()
-    return render(request, 'administracao.html')
+
+    context = {}
+    if request.method == 'POST':
+        form = CadastrarPartipantesForms(request.POST)
+        if not form.is_valid():
+            raise RuntimeError("algo errado, arruma esta resposta aqui")
+        form.save()
+        context['participantes'] = [p for p in CadastrarParticipantes.objects.all()]
+
+    context['form'] = CadastrarPartipantesForms()
+    return render(request, 'administracao.html', context)
