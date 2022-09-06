413442bfc0d88b597150bec1865e0a6a00b7a1a1
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2022-09-06 09:15:14 -0300

    removido textos

--- home/gerador_times.py
+++ home/gerador_times.py
@@ -4,7 +4,6 @@ import random
 # RAZOVEL = 3
 # BOM = 2
 # MUITO_BOM = 1
-# jogadores = {'Ana/Gu':2,'Aninha':1,'Daniel':1,'Erle':3,'Gi Rosa':2,'Ingrid':4,'Laura':2,'Lorrana':4,'Lu del Grande':2,'Mi Torres':3,'Nath':1,'Prata':3,'Vital':1,'Vitor': 1,'Edson': 1,'Iuri': 2,'Bia': 3,'Clara (irmã Bia)': 4,'Hiago': 1,'Vinicius Girotto (Bia)': 2,'Davi (Bia)':4,'aa':4, 'bb':4, 'cc':4}
 
 
 def montar_time(participantes, aleatoriedade=None):
--- home/views.py
+++ home/views.py
@@ -24,19 +24,3 @@ def jogador(request):
     }
     return render(request, 'home.html', context)
 
-
-    """
-                Niveis de habilidade:
-(1 = muito bom | 2 = bom | 3 = razoavel  | 4 = em aprendizado)
-------------------------------------------------------------------------"""
-"""
-{'nath': 1, 'bernardo': 1, 'denis': 2, 'aninha': 2,
-                     'gi rosa': 2, 'vital': 1, 'marcelo': 1, 'math': 2,
-                     'lu': 3, 'ana/gu': 2, 'mi': 3, 'gu gondim': 2, 'caue': 2,
-                     'lari rizzo': 4, 'erle': 3, 'prata': 3, 'daniel': 2,
-                     'marilia': 3, 'eliel': 2, 'lorrana': 4, 'amanda': 2,
-                     'ingrid': 4, 'laura': 3, 'felipe': 2}
-                     'Nicolle': 1 , 'Bernardo': 1,'Bia':2,'Denis':2,'Vital': 1,'Marcelo':1,'Math':2, 'lu': 3,
-            'Mi Torres': 3,'Erle':3 ,'Prata':3 ,'Daniel':2,'Marilia':2,'Ingrid':4 ,'Gabriela':3 ,'Laura': 3,'Natan':3 ,
-            'Eliel':2 ,'Lorrana':4 ,'Fabricio':1 ,'Gi Michelato':4 ,'Feza':2 ,'Edson':2
-"""
